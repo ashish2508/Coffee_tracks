c269e4e6707c65870e1c0cf91d9fec716bfc5274
ashish2508 <jha250805@gmail.com>
2024-12-21 03:07:00 +0530

    header and footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,33 @@
 export default function Layout(props) {
-  const {children}=props
+	const { children } = props;
 
-  const header={
-<header></header>
-  }
+	const header = (
+		<header>
+			<div className="">
+				<h1 className="text-gradient">CAFFIEND</h1>
+				<p>For Coffee Adects :)</p>
+			</div>
+			<button>
+				Sign Up free
+				<i className="fa-solid fa-mug-hot"></i>
+			</button>
+		</header>
+	);
 
-  const footer={
-<footer></footer>
-
-  }
-  return(
-    <>
-    {header}
-    <main>
-{children}
-    </main>
-    {footer}
-    </>
-  )
+	const footer = (
+		<footer>
+			<p>
+				<span className="text-gradient">Caffiend</span> was made by
+				<a href="https://github.com/ashish2508"> Ashish Jha </a>
+				 👈🏻(this guy got help)
+			</p>
+		</footer>
+	);
+	return (
+		<>
+			{header}
+			<main>{children}</main>
+			{footer}
+		</>
+	);
 }
