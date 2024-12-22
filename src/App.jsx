e3c5544e2f9313b25e169a77497ccdd04acb07a8
ashish2508 <jha250805@gmail.com>
2024-12-22 20:54:48 +0530

    nothing major

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Layout from "./components/Layout";
 import Hero from "./components/Hero"
 import CoffeeForm from "./components/CoffeeForm";
 import Stats from "./components/Stats";
-import History from "./components/History"; 
+import History from "./components/History";
 
 function App() {
-    const isAuthenticated=false;
+    const isAuthenticated=true;
     const authenticatedContent=(
       <>
   <Stats/>
