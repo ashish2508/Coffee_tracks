0088bfc669080aba381790ab5850c8d692e87998
ashish2508 <jha250805@gmail.com>
2024-12-23 07:20:36 +0530

    made it so it loads eevry 2 sec

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		watch: {
+			usePolling: true, // Enables polling for file changes
+			interval: 2000, // Adjust polling interval if necessary
+		},
+	},
+});
